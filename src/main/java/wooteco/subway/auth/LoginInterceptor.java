11c4bc672db251d85d6b4a023ed3d430af0399c5
yjksw <cairofaith@gmail.com>
2021-05-26 22:39:05 +0900

    feat: 인터셉터 cors 설정 추가

--- src/main/java/wooteco/subway/auth/LoginInterceptor.java
+++ src/main/java/wooteco/subway/auth/LoginInterceptor.java
@@ -1,5 +1,6 @@
 package wooteco.subway.auth;
 
+import org.springframework.http.HttpMethod;
 import org.springframework.web.servlet.HandlerInterceptor;
 import wooteco.subway.auth.application.AuthService;
 import wooteco.subway.auth.infrastructure.AuthorizationExtractor;
@@ -16,6 +17,10 @@ public class LoginInterceptor implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        if (HttpMethod.OPTIONS.equals(request.getMethod())) {
+            return true;
+        }
+
         authService.validateToken(AuthorizationExtractor.extract(request));
         return true;
     }
